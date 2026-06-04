028b33795209efabb5e402d5b2db98d8b8b6b23f
Torrey Atcitty <torrey@comp.xyz>
2026-06-04 16:54:41 -0700

    Removed debug logs.

--- scripts/release.js
+++ scripts/release.js
@@ -22,8 +22,6 @@ async function release(cid, url, signature) {
   try {
     json = await res.json();
   } catch (error) {
-    console.log("received response: ", res);
-    console.log('text response: ', res.text());
     throw new Error(`Response was not valid JSON: ${error}`);
   }
 
